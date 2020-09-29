2f281d460ab314873f2078e020325dffa074afcb
Svein-Kåre Bjørnsen <sveinkare@gmail.com>
2020-09-29 21:01:04 +0200

    removed commented code

--- src/InnLisp.java
+++ src/InnLisp.java
@@ -9,15 +9,6 @@ import java.io.InputStreamReader;
 
 public class InnLisp {
     public static void main(String[] args) throws IOException {
-        /*String code = "(+ \n\t(- 20 1 1 1 1 1 1 1 1 1 1) (* 5 3))";
-        //String code = "(/ 16 2 2)";
-
-        System.out.println("\nTesting interpreting:\n\n" + code + "\n");
-
-        Operand result = Parser.parse(code).interpret(null);
-
-        System.out.println("Result: " + result.getValue());*/
-
         repl();
     }
 
